743b4eaf978f0e40ec2621469a2d902f68695461
Makarov Igor <makarov_iv@directum.ru>
2021-12-24 21:55:01 +0400

    Исправлена ошибка, из-за которой изменения конфигов подхватывались не с первого раза

--- ManageAppProjects/map_plugin/map_installer.py
+++ ManageAppProjects/map_plugin/map_installer.py
@@ -5,6 +5,7 @@ import os.path
 from typing import Any, Optional
 import termcolor
 import pathlib
+import time
 
 from py_common import process
 from fire.formatting import Bold
@@ -18,6 +19,7 @@ from py_common.logger import log
 from sungero_deploy.deployment_tool import DeploymentTool
 from sungero_deploy.services.sungero_web_client import SungeroWebClient
 from common_plugin import yaml_tools, git_tools
+from sungero_deploy.scripts_config import get_config_model
 
 
 
@@ -45,27 +47,19 @@ class ManageAppliedProject(BaseComponent):
         log.info(f'"{self.__class__.__name__}" component has been successfully installed.')
         self._print_help_after_action()
 
+    def uninstall(self) -> None:
+        """
+        Удалить компоненту.
+        """
+        #self._static_controller.remove_static(self._static_url_path)
+        log.info(f'"{self.__class__.__name__}" component has been successfully uninstalled.')
+        self._print_help_after_action()
+
     def current(self) -> None:
         show_config(self.config_path)
 
     def check_config(self, config_path: str) -> None:
         show_config(config_path)
-
-    """
-    def test(self, src_config_path: str, dst_config_path: str) -> None:
-        pass
-        import pprint
-        pp = pprint.PrettyPrinter(indent=4)                
-        src_config = yaml_tools.load_yaml_from_file(src_config_path)
-        dst_config = yaml_tools.load_yaml_from_file(dst_config_path)
-        #dst_config["variables"]["repositories"]  = src_config["variables"]["repositories"].copy() 
-        dst_config["services_config"]["DevelopmentStudio"]['REPOSITORIES']["repository"]  = src_config["services_config"]["DevelopmentStudio"]['REPOSITORIES']["repository"].copy() 
-        repos_as_str =""
-        for repo in src_config["services_config"]["DevelopmentStudio"]['REPOSITORIES']["repository"]:
-            repos_as_str += f'"folder={repo.get("@folderName")}, type={repo.get("@solutionType")}, url={repo.get("@url")}" '
-        dst_config["variables"]["repositories"]  = repos_as_str
-        yaml_tools.yaml_dump_to_file(dst_config, dst_config_path+"_tmp")
-    """
 
     def set(self, project_config_path: str) -> None:
         while (True):
@@ -73,10 +67,12 @@ class ManageAppliedProject(BaseComponent):
             answ = input("Переключиться на указанный проект? (y,n):")
             if answ=='y' or answ=='Y':
                 # остановить сервисы
+                log.info(colorize("Остановка сервисов"))
                 all = All(self.config)
                 all.down()
 
                 # скорректировать etc\config.yml
+                log.info(colorize("Корректировка config.yml"))
                 src_config = yaml_tools.load_yaml_from_file(project_config_path)
                 dst_config = yaml_tools.load_yaml_from_file(self.config_path)
                 dst_config["services_config"]["DevelopmentStudio"]['REPOSITORIES']["repository"]  = src_config["services_config"]["DevelopmentStudio"]['REPOSITORIES']["repository"].copy() 
@@ -85,26 +81,25 @@ class ManageAppliedProject(BaseComponent):
                 dst_config["variables"]["home_path"] = src_config["variables"]["home_path"]
                 dst_config["variables"]["home_path_src"]  = src_config["variables"]["home_path_src"]
                 yaml_tools.yaml_dump_to_file(dst_config, self.config_path)
+                time.sleep(2)
 
                 # поднять сервисы
-                all2 = All(self.config)
+                log.info(colorize("Подъем сервисов"))
+                all2 = All(get_config_model(self.config_path))
                 all2.config_up()
                 all2.up()
 
                 # обновить конфиг DDS
+                log.info(colorize("Обновление конфига DDS"))
                 from dds_plugin.development_studio import DevelopmentStudio
                 DevelopmentStudio(self.config_path).generate_config_settings()
+
+                log.info("")
+                log.info(colorize("Новые параметры:"))
+                self.current()
                 break
             elif answ=='n' or answ=='N':
                 break
-
-    def uninstall(self) -> None:
-        """
-        Удалить компоненту.
-        """
-        #self._static_controller.remove_static(self._static_url_path)
-        log.info(f'"{self.__class__.__name__}" component has been successfully uninstalled.')
-        self._print_help_after_action()
 
     def generate_empty_project_config(self, new_config_path: str) -> None:
         template_config="""# ключевые параметры проекта
@@ -134,6 +129,7 @@ services_config:
         if not p_config_path.exists():
             with open(new_config_path, 'w', encoding='utf-8') as f:
                 f.write(template_config)
+            log.info(colorize(f'Создан файл описания проекта {new_config_path}.'))
         else:
             log.error(f'Файл {new_config_path} уже существует.')
 
@@ -144,9 +140,11 @@ services_config:
         log.info('do map set <путь к yml-файлу> - переключиться на проект, описаный в указанном yml-файла')
         log.info('do map generate_empty_project_config <путь к yml-файлу> - создаст заготовку для файла описания проекта')
 
+def colorize(x):
+    return termcolor.colored(x, color="green", attrs=["bold"])
+
 def show_config(config_path):
     config = yaml_tools.load_yaml_from_file(_get_check_file_path(config_path))
-    colorize = lambda x: termcolor.colored(x, color="green", attrs=["bold"])
     vars = config.get("variables")
     repos = config.get("services_config").get("DevelopmentStudio").get('REPOSITORIES').get("repository")
     maxlen = 0
